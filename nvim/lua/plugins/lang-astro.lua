dbc82b909415d4e7d7cab531343fc5504ec7e015
Dinesh Pandiyan <flexdinesh@gmail.com>
2025-11-04 16:21:03 +1100

    astro lsp

---
+++ nvim/lua/plugins/lang-astro.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "css",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        astro = {},
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.vtsls = opts.servers.vtsls or {}
+      opts.servers.vtsls.settings = opts.servers.vtsls.settings or {}
+
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@astrojs/ts-plugin",
+          location = LazyVim.get_pkg_path("astro-language-server", "/node_modules/@astrojs/ts-plugin"),
+          enableForWorkspaceTypeScriptVersions = true,
+        },
+      })
+    end,
+  },
+  {
+    "conform.nvim",
+    opts = function(_, opts)
+      if LazyVim.has_extra("formatting.prettier") then
+        opts.formatters_by_ft = opts.formatters_by_ft or {}
+        opts.formatters_by_ft.astro = { "prettier" }
+      end
+    end,
+  },
+}
