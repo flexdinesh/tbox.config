2b41b2159bf7dab1a572f2f6e0c5e9fa3759bf3f
Dinesh Pandiyan <flexdinesh@gmail.com>
2025-12-08 19:48:02 +1100

    ghostty and nvim colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "folke/tokyonight.nvim" },
+  { "nkxxll/ghostty-default-style-dark.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "catppuccin-mocha",
-      colorscheme = "tokyonight",
+      -- colorscheme = "tokyonight",
+      colorscheme = "ghostty-default-style-dark",
     },
   },
 }
